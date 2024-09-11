29d19bc6de8bf9571ff3349fcb1ac69ce8e24951
adam <delsarte.adam@gmail.com>
2024-09-11 11:23:59 +0200

    basic error handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -13,6 +14,13 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists.")
+
     store_id = uuid.uuid4().hex  # generate random string of letter and number
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
@@ -22,6 +30,13 @@ def create_store():
 @app.post("/item")  # name is pass from <string:name>
 def create_item():
     item_data = request.get_json()
+    if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
+        abort(400, message="Bad request. Ensure 'price' ,'store_id', and 'name' are included in the JSON payload")
+
+    for item in item.values():
+        if (item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
+            abort(400, message="item already exists.")
+
     if item_data["store_id"] not in stores:
         return {"message": "Store not found"}, 404
     item_id = uuid.uuid4().hex  # generate random string of letter and number
@@ -40,7 +55,7 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
@@ -48,4 +63,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
